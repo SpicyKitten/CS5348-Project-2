306ba5f9ffe01ab2a1d0d16f230692f337aede50
Avilash Rath <rathavilash@gmail.com>
2021-11-02 15:05:50 -0500

    Began work on openfs

--- filesys.cpp
+++ filesys.cpp
@@ -1,10 +1,31 @@
 #include "filesys.hpp"
 
+FileSystem::~FileSystem()
+{
+    std::cout << "~FileSystem" << std::endl;
+}
 void FileSystem::initfs(unsigned long totalBlocks, unsigned long iNodeBlocks)
 {
     std::cout << "Executing initfs " << totalBlocks << " " << iNodeBlocks << std::endl;
+    // delete the old i-nodes
+    this->_inodes.clear();
+    // total size = totalBlocks * block size = totalBlocks * 1024 bytes
+    // i-nodes = 16 i-nodes per block
+    // total i-nodes = 16 * iNodeBlocks
+    // block structure:
+    // ______________________________________________________________________________________
+    // | boot info | superblock | ... i-node blocks ... |        ... data blocks ...        |
+    // |     0     |     1      | 2 to iNodeBlocks + 1  | iNodeBlocks + 2 to totalBlocks - 1|
+    // ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ ̄ 
+    this->_inodes.push_back(std::unique_ptr<INode>(new INode()));
 }
 void FileSystem::openfs(std::string filename)
 {
     std::cout << "Executing openfs " << filename << std::endl;
+    // create a file if it doesn't exist
+    // open the file otherwise
+    auto fd = open(filename.c_str(), O_CREAT | O_RDWR);
+    write(fd, filename.c_str(), sizeof(filename.c_str()));
+    close(fd);
+    std::cout << "Obtained file descriptor: " << fd << std::endl;
 }
--- filesys.hpp
+++ filesys.hpp
@@ -1,10 +1,19 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <memory>
+#include <vector>
+#include "inode.hpp"
+#include <fcntl.h>
+#include <unistd.h>
 
 class FileSystem
 {
+private:
+
+    std::vector<std::unique_ptr<INode>> _inodes;
 public:
+    ~FileSystem();
     void openfs(std::string filename);
     void initfs(unsigned long totalBlocks, unsigned long iNodeBlocks);
 };
